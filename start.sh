1b5d426f85d3af309cfe4226f8f6d6c96135a8cd
Praveer Kumar <praveerkumar17@gmail.com>
2022-05-19 01:07:04 +0200

    Deploy mlflow 9

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ set -e
 export BACKEND_STORE_URI=postgresql://$DB_USER:$DB_PASS@/$DB_NAME?unix_socket=/cloudsql/opendatasets-349618:europe-west3:ml-flow-database 
 mlflow ui --host 0.0.0.0 --port 8080 --backend-store-uri=${BACKEND_STORE_URI} --default-artifact-root=${DEFAULT_ARTIFACT_ROOT} &
 while ! nc -z localhost 8080 ; do sleep 1 ; done
-/oauth2-proxy/oauth2-proxy --upstream=http://localhost:8080 --config=${OAUTH_PROXY_CONFIG} --http-address=0.0.0.0:8080 &
+/oauth2-proxy/oauth2-proxy --upstream=http://localhost:8080 --config=${OAUTH_PROXY_CONFIG} --http-address=0.0.0.0:4130 &
 
 wait -n
